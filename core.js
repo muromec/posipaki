9502bbf6a001c8bb9b6c0676c0a870e6327cdbd9
Illya Petrov <ilya.muromec@gmail.com>
2023-02-14 23:58:03 +0100

    another example with implicit exit

--- core.js
+++ core.js
@@ -2,34 +2,47 @@ function spawn(fn, pname, toParent = ()=> null) {
   let current = null;
   let buffer = [];
   let resolveExit;
+  let children = [];
   let exitPromise = new Promise(resolve => {
     resolveExit = resolve;
   });
 
   function send ({ to = pparent, ...msg }) {
-    let ret = to.next({ msg });
-    tick();
-    if (ret.done) {
-      resolveExit();
-    }
+    tick(to.next({ msg }));
   }
-  function tick () {
+
+  function tick (ret) {
     let msg;
     while(msg = buffer.pop()) {
-      toParent({pname, ...msg});
+      toSelf({pname, ...msg});
+    }
+    if (ret && ret.done) {
+      resolveExit();
     }
   }
   function toSelf(msg) {
     send({ to: current, ...msg});
   }
-  function fromChild(msg) {
-    toSelf(msg);
-  }
   function toBuffer(msg) {
     buffer.push(msg);
+    setImmediate(tick);
   }
+  function fromChild(msg) {
+    if (msg.type === 'EXIT') {
+      children = children.filter(p=> p !== msg.pid);
+    }
+    toBuffer(msg);
+  }
+  function toAllChildren(msg) {
+    children.forEach(p => p.send(msg));
+  }
+
   function fork (fn, pname) {
-    return spawn(fn, pname, fromChild);
+    return (...args) => {
+      const child =  spawn(fn, pname, fromChild)(...args);
+      children.push(child);
+      return child;
+    }
   }
   function wait () {
     return exitPromise;
@@ -37,9 +50,10 @@ function spawn(fn, pname, toParent = ()=> null) {
   return (...args) => {
     const process = {
       pname,
-      send: toSelf,
+      send: toBuffer,
       fork,
-      toParent: toBuffer,
+      toParent,
+      toAllChildren,
       id: Symbol(pname),
       state: null,
       wait,
@@ -48,7 +62,7 @@ function spawn(fn, pname, toParent = ()=> null) {
     current = task;
     let ret = task.next();
     process.state = ret.value;
-    task.next();
+    tick(task.next());
 
     return process;
   }
@@ -57,15 +71,22 @@ function spawn(fn, pname, toParent = ()=> null) {
 function watchExit(fn) {
   return function* (process, ...args) {
     yield* fn(process, ...args);
+    process.toAllChildren({ type: 'STOP' });
     process.toParent({ type: 'EXIT', pid: process.id});
   }
 }
 
-function* runDispatch(name, fn, readyFn = ()=> false) {
+function debugLog(level, ...args) {
+  if (level) {
+    console.log(...args);
+  }
+}
+
+function* runDispatch(name, fn, readyFn = ()=> false, debugLevel = false) {
   let msg;
   while(!readyFn()) {
     ({msg} = yield);
-    console.log('msg', name, ' <- ', msg);
+    debugLog(debugLevel, 'msg', name, ' <- ', msg);
     fn(msg);
   }
 }
---
+++ test2.js
@@ -0,0 +1,45 @@
+import { spawn, runDispatch } from './core.js';
+
+const timer = setTimeout(()=> null, 1000 * 30);
+
+function* fast({ pname }) {
+  const state = true;
+  yield state;
+  yield* runDispatch(pname, msg => {
+  }, ()=> state, true)
+}
+
+function* slow({ pname, send }) {
+  const state = { done: false }
+  yield state;
+
+  const timer = setTimeout(() => {
+    send({ type: 'FIRED'})
+  }, 10 * 1000);
+
+  yield* runDispatch(pname, msg => {
+    state.done = true;
+  }, ()=> state.done, true)
+
+  clearTimeout(timer);
+}
+
+function* main({ pname, fork }) {
+  const state = { done: false };
+  yield state;
+  const child = fork(fast, 'f1')();
+  const timer = fork(slow, 'timer')();
+
+  yield* runDispatch(pname, (msg) => {
+    if (msg.type === 'EXIT' && msg.pid === child.id) {
+      // state.done = true;
+    }
+    if (msg.type === 'EXIT' && msg.pid === timer.id) {
+      state.done = true;
+    }
+  }, ()=> state.done, true);
+}
+
+const m = spawn(main, 'main')();
+await m.wait();
+clearTimeout(timer);
