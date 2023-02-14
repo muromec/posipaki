03cc6b1d9af96fefdbde594fc0ea0521f8109f6c
Illya Petrov <ilya.muromec@gmail.com>
2023-02-15 00:43:38 +0100

    add pipe example

---
+++ pipe.js
@@ -0,0 +1,47 @@
+import { runDispatch } from './core.js';
+
+function pipe(fns) {
+  return function* ({ pname, fork }, params) {
+    const state = { params: { ...params }, result: null, running: false};
+    yield state;
+
+    const toRun = [...fns];
+    function isDone() {
+      return !state.running;
+    }
+
+    let task;
+    function spawnNext(params) {
+      const currentFn = toRun.shift();
+      const name = pname + ` [${currentFn.name}]`;
+      task = fork(currentFn, name)(state.params);
+    }
+
+    function hasNext() {
+      return toRun.length > 0;
+    }
+
+    spawnNext();
+
+    state.running = hasNext();
+    yield* runDispatch(pname, msg => {
+      if (msg.type === 'EXIT' && msg.pid === task.id) {
+        if (hasNext()) {
+          state.params = { ...state.params, ...task.state.result };
+          spawnNext();
+        } else {
+          console.log('last one is gone');
+          state.running = false;
+          state.result = task.state.result;
+        }
+      }
+      if (msg.type === 'STOP') {
+        state.params = null;
+        state.running = false;
+      }
+    }, isDone, true);
+  };
+}
+
+
+export { pipe };
---
+++ test3.js
@@ -0,0 +1,39 @@
+import { spawn, runDispatch } from './core.js';
+import { pipe } from './pipe.js';
+
+const timer = setTimeout(()=> null, 1000 * 30);
+
+
+function* f1({ pname, send }, { number }) {
+  const state = { done: false, result: null };
+  yield state;
+  console.log('run f1', number);
+  const timer = setTimeout(()=> send({ type: 'POKE'}), 100 * number);
+  yield* runDispatch(pname, msg => {
+      state.done = true;
+      state.result = { number: number * 2 };
+  }, ()=> state.done, true);
+  clearTimeout(timer);
+  console.log('gone');
+}
+
+const f2 = f1;
+const f3 = f1;
+
+function* main({ pname, fork }) {
+  const state = { done: false };
+  yield state;
+  const worker = fork(pipe([f1, f2, f3]), 'w')({ number: 1 });
+
+  yield* runDispatch(pname, (msg) => {
+    if (msg.type === 'EXIT' || msg.type === 'STOP') {
+      state.done = true;
+    }
+  }, ()=> state.done, true);
+  console.log('worker result', worker.state.result);
+}
+
+const m = spawn(main, 'main')();
+setTimeout(()=> m.send({ type: 'STOP' }), 300);
+await m.wait();
+clearTimeout(timer);
