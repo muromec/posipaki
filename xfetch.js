1071e20306283c84787e60b0b2fe0dc77ee16290
Illya Petrov <ilya.muromec@gmail.com>
2023-06-13 03:45:06 +0200

    keep state in xfetch after it exits

--- xfetch.js
+++ xfetch.js
@@ -1,7 +1,12 @@
 import { runDispatch } from './core.js';
 
+function isJsonHelper(res) {
+  const ct = res.headers.get('content-type');
+  return ct === 'application/json';
+}
+
 function* xfetch({ pname, toParent, send: toSelf }, { url }) {
-  const state = { code: 'pending' };
+  const state = { code: 'pending', data: null, text: null };
   yield state;
 
   const controller = new AbortController();
@@ -9,9 +14,15 @@ function* xfetch({ pname, toParent, send: toSelf }, { url }) {
 
   (async function do_request() {
     try {
+      toSelf({ type: 'LOADING'});
       const res = await fetch(url.href, { signal });
-      const text = await res.text();
-      toSelf({ type: 'OK', text });
+      if (isJsonHelper(res)) {
+        const data = await res.json();
+        toSelf({ type: 'OK', data });
+      } else {
+        const text = await res.text();
+        toSelf({ type: 'OK', text });
+      }
     } catch (e) {
       const isAborted = (e instanceof DOMException && e.name === 'AbortError');
       if (isAborted) {
@@ -23,9 +34,10 @@ function* xfetch({ pname, toParent, send: toSelf }, { url }) {
     }
   })();
 
+  const isDone = ()=> !(state.code === 'pending' || state.code === 'loading')
+
   yield* runDispatch(pname, (msg)=> {
     if (msg.type === 'STOP') {
-      console.log('abort');
       controller.abort();
     }
     if (msg.type === 'ABORTED') {
@@ -36,11 +48,17 @@ function* xfetch({ pname, toParent, send: toSelf }, { url }) {
       toParent(msg);
       state.code = 'failed';
     }
+    if (msg.type === 'LOADING') {
+      state.code = 'loading';
+    }
+
     if (msg.type === 'OK') {
       toParent(msg);
       state.code = 'ok';
+      state.data = msg.data;
+      state.text = msg.text;
     }
-  }, ()=> state.code !== 'pending', true);
+  }, isDone, true);
 }
 
 export { xfetch };
