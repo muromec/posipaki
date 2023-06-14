23d100bbe2854369fe0736511dc6a257fb44f017
Illya Petrov <ilya.muromec@gmail.com>
2023-06-14 10:28:43 +0200

    put with xfetch

--- src/xfetch.ts
+++ src/xfetch.ts
@@ -11,9 +11,18 @@ export type FetchState<T> = {
   data: T | null,
   text: string | null,
 };
-export type FetchArgs = {
+type FetchArgsGet = {
   url: URL,
+  method?: 'GET',
+  body?: undefined,
 };
+type FetchArgsSend<T> = {
+  url: URL,
+  method?: 'POST' | 'PUT' | 'PATCH',
+  body: T,
+};
+export type FetchArgs<T> = FetchArgsGet | FetchArgsSend<T>;
+
 type FetchMessage<T> = {
   type: string,
   data?: T | null,
@@ -22,7 +31,7 @@ type FetchMessage<T> = {
 
 type FetchGenerator<T> = Generator<FetchState<T> | null, void, Message>;
 
-function* xfetch<Type>({ pname, toParent, send } : ProcessCtx, { url } : FetchArgs) : FetchGenerator<Type> {
+function* xfetch<Type>({ pname, toParent, send } : ProcessCtx, { method='GET', url, body } : FetchArgs<Type>) : FetchGenerator<Type> {
   const state: FetchState<Type> = { code: 'pending', data: null, text: null };
   yield state;
 
@@ -34,7 +43,12 @@ function* xfetch<Type>({ pname, toParent, send } : ProcessCtx, { url } : FetchAr
   (async function do_request() {
     try {
       toSelf({ type: 'LOADING'});
-      const res: Response = await fetch(url.href, { signal });
+      const serializedBody = method === 'GET' ? undefined : JSON.stringify(body);
+      const headers = new Headers({});
+      if (serializedBody) {
+        headers.set('content-type', 'application/json');
+      };
+      const res: Response = await fetch(url.href, { method, signal, body: serializedBody, headers });
       if (isJsonHelper(res)) {
         const data = await res.json();
         toSelf({ type: 'OK', data });
